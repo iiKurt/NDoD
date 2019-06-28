7795eb483698efe3fffdb3a755cdbf7ed71b9bd8
iiKurt <ikurt018@gmail.com>
2019-06-28 14:03:24 +1200

    Setup window closes instead of hides

--- NDoD/NDoD/Setup.xaml.cs
+++ NDoD/NDoD/Setup.xaml.cs
@@ -19,7 +19,7 @@ namespace NDoD
             if (ValidateCaseCountTextBox())
             {
                 Window window = new MainWindow(int.Parse(CaseCountTextBox.Text));
-                this.Hide();
+                this.Show();
                 window.Show();
             }
         }
